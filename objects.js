ade4ea5b079efcb02a47d0d71720c8682b57fc24
Elaine Thai <ethai1827@gmail.com>
2019-05-14 21:50:03 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  SHINee: "View"
+}
+
+function updatePlaylist(playlist, artist, title) {
+  return Object.assign({}, playlist, {artist: title})
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist;
+}
